1fbbfc53e6411b835d4d78b9757d690a87b35a6b
patriciarealini <hello@patriciarealini.com>
2016-04-22 18:37:31 -0700

    add in a boilerplate Example component to provide a framework for building future components with this boilerplate set up

---
+++ client/components/Example/index.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import Radium from "radium";
+
+class Example extends Component {
+
+  render () {
+
+    return (
+      <div>
+        <h1>Example Component</h1>
+      </div>
+    );
+  }
+};
+
+export default Example;
