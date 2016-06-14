2f76c54ebcb770a7e05bf2ce35c5bcf3e5538b50
patriciarealini <hello@patriciarealini.com>
2016-06-13 19:00:09 -0700

    sample radium css inline styles & sample props.

--- client/components/Example/index.jsx
+++ client/components/Example/index.jsx
@@ -1,15 +1,31 @@
-import React, {Component} from "react";
+import React, {Component, PropTypes} from "react";
 import Radium from "radium";
 
+@Radium
 class Example extends Component {
+
+  static propTypes = {
+    exampleProps: PropTypes.string
+  }
 
   render () {
 
     return (
-      <div>
-        <h1>Example Component</h1>
+      <div style={styles.example}>
+        <h1 style={styles.h1Example}>Example Component</h1>
+        <p>{this.props.exampleProps}</p>
       </div>
     );
+  }
+};
+
+var styles = {
+  example: {
+    backgroundColor: "Tomato",
+    border: "10px solid Firebrick"
+  },
+  h1Example: {
+    color: "moccasin"
   }
 };
 
